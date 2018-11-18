d3c4f52d4e83a4b5c89cb77be51cf05204a68b7e
malikperang@gmail.com <malikperang@gmail.com>
2018-11-19 02:24:06 +0800

    add nginx

--- lemp_stack/recipes/deploy.rb
+++ lemp_stack/recipes/deploy.rb
@@ -4,16 +4,46 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+include_recipe "apt"
 
+package 'nginx' do
+  action :install
+end
 
-package node['hello_web']['package']
+service 'nginx' do
+  action [ :enable, :start ]
+end
 
-cookbook_file "#{node['hello_web']['docroot']}/index.html" do
-  source 'index.html'
+template "/etc/nginx/sites-available/default.conf" do
+  source "default.conf.erb"
   action :create
 end
 
-service node['hello_web']['service'] do
-  supports status: true, restart: true, reload: true
-  action %i(enable start)
+execute "enable-site" do
+  command "sudo ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf"
 end
+
+cookbook_file "/var/www/html/index.html" do
+  source "index.html"
+  mode "0644"
+end
+
+# apt_update
+#
+# package node['lemp_stack']['package'] do
+#   action :install
+# end
+#
+# cookbook_file "#{node['lemp_stack']['docroot']}/index.html" do
+#   source 'index.html'
+#   action :create
+# end
+#
+# service node['lemp_stack']['service'] do
+#   action [:enable, :start]
+# end
+
+# service node['lemp_stack']['service'] do
+#   supports status: true, restart: true, reload: true
+#   action %i(enable start)
+# end
