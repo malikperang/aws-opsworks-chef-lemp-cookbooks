1cb5118fb0422283f86a451e101cbe04ab48ada8
malikperang@gmail.com <malikperang@gmail.com>
2018-11-19 10:23:08 +0800

    change apt cookbook dependency

--- apt/metadata.rb
+++ apt/metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures apt'
 long_description 'Installs/Configures apt'
 version '0.1.0'
-chef_version '>= 13.0'
+chef_version '>= 12.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
